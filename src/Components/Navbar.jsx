96988b853a299df10e34e3de21c19322396de0b5
Saras <78587510+sarasverma@users.noreply.github.com>
2023-05-10 11:06:02 +0530

    Navbar done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useRef } from "react";
+import logo from "../assets/logo.png";
+import { ImCross } from "react-icons/im";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
-  return <div></div>;
+  const navLinks = useRef(null);
+
+  return (
+    <nav className="flex justify-between items-center h-16 px-16 max-sm:px-8">
+      <div className="logo">
+        <img src={logo} alt="logo" className="w-8 aspect-square" />
+      </div>
+      <div
+        ref={navLinks}
+        className="nav-links max-sm:fixed max-sm:top-0 max-sm:left-0 max-sm:w-[100%] max-sm:h-[100%] max-sm:grid max-sm:place-items-center transition-[transform] duration-[0.5s] max-sm:translate-x-[-100vw] max-sm:bg-orange-500	"
+      >
+        <ul className="flex gap-5 max-sm:flex-col max-sm:items-center max-sm:justify-center">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Services</a>
+          </li>
+          <li>
+            <a href="/">Login</a>
+          </li>
+        </ul>
+        <button
+          className="hidden max-sm:block max-sm:absolute top-10 right-10"
+          onClick={() => {
+            if (
+              !navLinks.current.classList.contains(
+                "max-sm:translate-x-[-100vw]"
+              )
+            )
+              navLinks.current.classList.add("max-sm:translate-x-[-100vw]");
+          }}
+        >
+          <ImCross />
+        </button>
+      </div>
+      <div className="toogleNav hidden max-sm:block">
+        <button
+          className="max-sm:block"
+          onClick={() => {
+            if (
+              navLinks.current.classList.contains("max-sm:translate-x-[-100vw]")
+            )
+              navLinks.current.classList.remove("max-sm:translate-x-[-100vw]");
+          }}
+        >
+          <GiHamburgerMenu />
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
