930c325dc456176c46972433fc47a00740786aea
Saras <78587510+sarasverma@users.noreply.github.com>
2023-05-16 23:27:27 +0530

    navbar bugs fixed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,11 @@
-import React, { useContext, useRef } from 'react';
-import logo from '../assets/logo.png';
-import { ImCross } from 'react-icons/im';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { Link, useLocation } from 'react-router-dom';
-import { signOut } from 'firebase/auth';
-import { auth } from '../firebase';
-import { AuthContext } from '../context/AuthContext';
+import React, { useContext, useRef } from "react";
+import logo from "../assets/logo.png";
+import { ImCross } from "react-icons/im";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Link, useLocation } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
   const location = useLocation();
@@ -13,12 +13,17 @@ const Navbar = () => {
 
   const { currentUser } = useContext(AuthContext);
 
-  const handleSignOut = () => {
-    signOut(auth);
-    alert('Logout successfully !');
+  const handleCloseNav = () => {
+    if (!navLinks.current.classList.contains("max-sm:translate-x-[-100vw]"))
+      navLinks.current.classList.add("max-sm:translate-x-[-100vw]");
   };
 
-  if (location.pathname === '/auth') return <></>;
+  const handleSignOut = () => {
+    signOut(auth);
+    alert("Logout successfully !");
+  };
+
+  if (location.pathname === "/auth") return <></>;
 
   return (
     <nav className="flex justify-between items-center h-16 px-16 max-sm:px-8 sticky bg-slate-400 max-sm:z-50">
@@ -27,37 +32,56 @@ const Navbar = () => {
       </div>
       <div
         ref={navLinks}
-        className="nav-links max-sm:fixed msamax-sm:top-0 max-sm:left-0 max-sm:w-[100%] max-sm:h-[100%] max-sm:grid max-sm:place-items-center transition-[transform] duration-[0.5s] max-sm:translate-x-[-100vw] max-sm:bg-orange-500">
+        className="nav-links max-sm:fixed max-sm:top-0 max-sm:left-0 max-sm:w-[100%] max-sm:h-[100%] max-sm:grid max-sm:place-items-center transition-[transform] duration-[0.5s] max-sm:translate-x-[-100vw] max-sm:bg-orange-500"
+      >
         <ul className="flex gap-5 max-sm:flex-col max-sm:items-center max-sm:justify-center">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" onClick={handleCloseNav} className="max-sm:text-2xl">
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link
+              to="/about"
+              onClick={handleCloseNav}
+              className="max-sm:text-2xl"
+            >
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/services">Services</Link>
+            <Link
+              to="/services"
+              onClick={handleCloseNav}
+              className="max-sm:text-2xl"
+            >
+              Services
+            </Link>
           </li>
           {currentUser === null ? (
             <li>
-              <Link to="/auth">Login</Link>
+              <Link to="/auth" className="max-sm:text-2xl">
+                Login
+              </Link>
             </li>
           ) : (
             <li>
-              <button onClick={handleSignOut}>LogOut</button>
+              <button
+                onClick={() => {
+                  handleSignOut();
+                  handleCloseNav();
+                }}
+                className="max-sm:text-2xl"
+              >
+                LogOut
+              </button>
             </li>
           )}
         </ul>
         <button
           className="hidden max-sm:block max-sm:absolute top-10 right-10"
-          onClick={() => {
-            if (
-              !navLinks.current.classList.contains(
-                'max-sm:translate-x-[-100vw]'
-              )
-            )
-              navLinks.current.classList.add('max-sm:translate-x-[-100vw]');
-          }}>
+          onClick={handleCloseNav}
+        >
           <ImCross />
         </button>
       </div>
@@ -66,10 +90,11 @@ const Navbar = () => {
           className="max-sm:block"
           onClick={() => {
             if (
-              navLinks.current.classList.contains('max-sm:translate-x-[-100vw]')
+              navLinks.current.classList.contains("max-sm:translate-x-[-100vw]")
             )
-              navLinks.current.classList.remove('max-sm:translate-x-[-100vw]');
-          }}>
+              navLinks.current.classList.remove("max-sm:translate-x-[-100vw]");
+          }}
+        >
           <GiHamburgerMenu />
         </button>
       </div>
